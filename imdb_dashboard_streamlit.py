75b65c5ae6400f60b36bb0430c1028c65a53db04
Sarah "Rei" Aisyah <180937082+sarahreiaisyah@users.noreply.github.com>
2025-06-13 06:22:45 +0700

    Update imdb_dashboard_streamlit.py

--- imdb_dashboard_streamlit.py
+++ imdb_dashboard_streamlit.py
@@ -243,11 +243,15 @@ st.plotly_chart(fig2, use_container_width=True)
 # Top 10 Film Terlaris berdasarkan genre yang dipilih
 if selected_genre != "Semua Genre":
     st.subheader(f"🏆 Top 10 Film Terlaris dalam Genre '{selected_genre}'")
+
     top_genre = exploded[exploded['genres'] == selected_genre]
     top_genre = top_genre.sort_values(by='numVotes', ascending=False).drop_duplicates('title')
     top_genre['year'] = top_genre['year'].astype(int)
     top_genre['rating'] = top_genre['rating'].astype(float).round(1)
-    top_genre['year'] = top_genre['year'].astype(int)
+
+    top_genre_display = top_genre[['title', 'year', 'rating', 'numVotes']].head(10)
+    st.table(top_genre_display)
+
 
 # Footer
 st.markdown("---")
